b0c27ac1d6afcc59f31d47cb567ed789a5038ab7
HEEKWON <higun295@gmail.com>
2022-01-14 19:42:34 +0900

    20220114_study

---
+++ LinkedList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LinkedList {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+
+    public class SinglyLinkedListNode<T> {
+        public T Data { get; set; }
+        public SinglyLinkedListNode<T> Next { get; set; }
+
+        public SinglyLinkedListNode(T data) {
+            this.Data = data;
+            this.Next = null;
+        }
+    }
+
+    public class SinglyLinkedList<T> {
+        private SinglyLinkedListNode<T> head;
+
+        public void Add(SinglyLinkedListNode<T> newNode) {
+
+        }
+    }
+}
